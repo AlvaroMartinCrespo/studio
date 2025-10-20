67817e5052d6b8b172b2a17e99b3d7975f4f6f0c
Alvaro Martin <alvaro.martin.crespo.00@gmail.com>
2025-10-20 15:25:46 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4710656589-d399e",
+  "appId": "1:966062331781:web:bf02ef9031a8d30caf56ec",
+  "apiKey": "AIzaSyAV4ex6U9kkZNp9vu-UYcurboJMAn53J0w",
+  "authDomain": "studio-4710656589-d399e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "966062331781"
+};
